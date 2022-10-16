687997343a4a6dcbc53d6c164442d16bcd3396ff
pretzelogic <yingyangyo266@gmail.com>
2022-10-16 09:40:26 -0500

    add game logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,25 +1,79 @@
 const gameInfo = (() => {
-  const board = document.getElementsByClassName("boardSpace");
+  const view = document.getElementsByClassName("boardSpace");
   const logBoard = () => {
     console.log(board);
   };
-  return { board, logBoard };
+
+  // const clearBoard = () => {
+  //   view[0].textContent = "";
+  //   view[1].textContent = "";
+  //   view[2].textContent = "";
+  //   view[3].textContent = "";
+  //   view[4].textContent = "";
+  //   view[5].textContent = "";
+  //   view[6].textContent = "";
+  //   view[7].textContent = "";
+  // };
+  
+  const getState = () => {
+    return [
+      [view[0].textContent, view[1].textContent, view[2].textContent],
+      [view[3].textContent, view[4].textContent, view[5].textContent],
+      [view[6].textContent, view[7].textContent, view[8].textContent]
+    ]
+  }
+  // view.forEach(button => {
+  //   button.addEventListener("click", updateSpace(button, ))
+  // })
+  
+  const updateSpace = (x, y) => {
+    x.textContent = y;
+  }
+  
+  return {logBoard, getState, updateSpace};
+})();
+
+const viewController = (() => {
+  const view = document.getElementsByClassName("boardSpace");
+  const clearBoard = () => {
+    view[0].textContent = "";
+    view[1].textContent = "";
+    view[2].textContent = "";
+    view[3].textContent = "";
+    view[4].textContent = "";
+    view[5].textContent = "";
+    view[6].textContent = "";
+    view[7].textContent = "";
+  };
+
+  return {clearBoard}
 })();
 
 const gameController = (() => {
-  const clearBoard = () => {
-    gameInfo.board[0].textContent = "";
-    gameInfo.board[1].textContent = "";
-    gameInfo.board[2].textContent = "";
-    gameInfo.board[3].textContent = "";
-    gameInfo.board[4].textContent = "";
-    gameInfo.board[5].textContent = "";
-    gameInfo.board[6].textContent = "";
-    gameInfo.board[7].textContent = "";
-  };
-  return { clearBoard };
+  let isInProgress = true
+  let currentPlayer = ""
+
+  const getStartPlayer = () =>{
+    if (Math.floor(Math.random() * 2) == 0){
+      return "X"
+    }else{
+      return "O"
+    }
+  }
+
+  const initializeGame = () => {
+    currentPlayer = getStartPlayer();
+  }
+
+  return {getStartPlayer, initializeGame, currentPlayer};
 })();
+
+const spaceButton = document.getElementsByClassName("boardSpace")
+// spaceButton.forEach(button => {
+//   button.addEventListener("click", )
+// })
+
 const resetButton = document.querySelector(".reset")
-resetButton.addEventListener("click", () => {
-    gameController.clearBoard()
-})
+resetButton.addEventListener("click", viewController.clearBoard())
+
+gameController.initializeGame()
